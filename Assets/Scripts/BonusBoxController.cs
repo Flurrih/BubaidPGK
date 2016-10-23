9dde8f69ea881704d321de5e50a23fe4f2f4ef3d
Marcin <jamesjohny11@gmail.com>
2016-10-23 14:06:54 +0200

    Added bonus boxes (two types with temporary speed bonus and health debuff).

---
+++ Assets/Scripts/BonusBoxController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BonusBoxController : MonoBehaviour {
+
+    private int durability;
+
+	// Use this for initialization
+	void Start ()
+    {
+        durability = 50;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void GotHit(Collider other)
+    {
+        durability -= 25;
+        //Debug.Log(durability);
+        if(durability <= 0)
+        {
+            //Destroy(transform.parent.gameObject);
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    public int GetDurability()
+    {
+        return durability;
+    }
+}
---
+++ Assets/Scripts/DurabilityTextController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DurabilityTextController : MonoBehaviour {
+
+    public GameObject obj;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // var rotationAngle = Quaternion.LookRotation(Camera.main.transform.position - transform.position); // we get the angle has to be rotated
+        //transform.localRotation = Quaternion.Slerp(transform.rotation, rotationAngle, Time.deltaTime * 5); // we rotate the rotationAngle 
+        // transform.position = GetComponentInParent<PlayerController>().transform.position;
+        transform.position = new Vector3(GetComponentInParent<BonusBoxController>().transform.position.x - 1, GetComponentInParent<BonusBoxController>().transform.position.y + 3, GetComponentInParent<BonusBoxController>().transform.position.z + 1);
+        //transform.LookAt(-Camera.main.transform.position);
+        GetComponent<TextMesh>().text = GetComponentInParent<BonusBoxController>().GetDurability().ToString();
+    }
+}
