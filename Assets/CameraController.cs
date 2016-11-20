16c596a38890cc740d004a252efe7c77b0b44f13
lorthal <bkluchcinski@gmail.com>
2016-11-20 01:53:57 +0100

    added moving camera for testing

---
+++ Assets/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField]
+    GameObject[] players;
+
+    private Vector3 targetPoint;
+	
+	void Update () {
+
+        float x=0, y=0, z=0;
+        int i = 0;
+
+        foreach (GameObject player in players)
+        {
+            if(player != null)
+            x = x + player.transform.position.x;
+            y = y + player.transform.position.y;
+            z = z + player.transform.position.z;
+            i++;
+        }
+
+        targetPoint = new Vector3(x / i, y / i, z / i);
+
+        transform.LookAt(targetPoint);
+    }
+}
