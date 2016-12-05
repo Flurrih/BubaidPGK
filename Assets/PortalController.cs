4840771d1eb4c86cfef7a1e838d27501e8493288
Flurrih <ghost744@gmail.com>
2016-12-06 00:14:47 +0100

    Added portal prefab, its meh but working better than expected.

---
+++ Assets/PortalController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalController : MonoBehaviour {
+
+    public GameObject otherPortal;
+    public GameObject playerDest, ballDest;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            //col.transform.position = otherPortal.GetComponent<PortalController>().playerDest.transform.position;
+            //col.GetComponent<PlayerController>().playersBall.transform.position = otherPortal.GetComponent<PortalController>().ballDest.transform.position;
+            col.transform.parent.position = otherPortal.GetComponent<PortalController>().playerDest.transform.position;
+        }
+
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
