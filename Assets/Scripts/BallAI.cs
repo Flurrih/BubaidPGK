34289707f46c2a8c807b69d2691b68e42afc3412
Flurrih <ghost744@gmail.com>
2016-12-21 11:08:00 +0100

    Podstawowe BallAI

---
+++ Assets/Scripts/BallAI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallAI : MonoBehaviour {
+
+    Transform targetPlayer; //the enemy's target
+    float moveSpeed = 3; //move speed
+    float rotationSpeed = 3; //speed of turning
+
+
+    Transform myTransform; //current transform data of this enemy
+
+
+    void Awake()
+    {
+        myTransform = transform; //cache transform data for easy access/preformance
+    }
+
+
+    void Start()
+    {
+        targetPlayer = GameObject.FindWithTag("Player").transform; //target the player
+
+    }
+
+
+    void Update()
+    {
+        if (Vector3.Distance(myTransform.position, targetPlayer.position) > 1.5f)
+        {
+            //rotate to look at the player
+            myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+            Quaternion.LookRotation(targetPlayer.gameObject.transform.position - myTransform.position), rotationSpeed * Time.deltaTime);
+
+
+            //move towards the player
+            myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+        }
+
+
+    }
+}
