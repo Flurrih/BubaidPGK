3b2f61de61018fb28a6a88c3e93647121a708967
lorthal <bkluchcinski@gmail.com>
2017-01-24 03:35:43 +0100

    audio for end game

--- Assets/MenuAudioController.cs
+++ Assets/MenuAudioController.cs
@@ -4,19 +4,29 @@ using System.Collections;
 public class MenuAudioController : MonoBehaviour {
 
     AudioSource audioSource;
-
+    bool gameOver;
 	void Start () {
         audioSource = GetComponent<AudioSource>();
         audioSource.time = 2.22f;
         audioSource.Play();
-	
+        gameOver = false;
 	}
 
     void Update()
     {
-        if(audioSource.time >= 147.81f)
+        if (!gameObject)
         {
-            audioSource.time = 14.76f;
+            if (audioSource.time >= 147.81f)
+            {
+                audioSource.time = 14.76f;
+            }
         }
+    }
+
+    public void GameOverSound()
+    {
+        gameOver = true;
+        audioSource.loop = false;
+        audioSource.time = 153.30f;
     }
 }
